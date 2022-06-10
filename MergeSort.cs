1b2dc698372aea643286e7196b3f2ec70135bcaf
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-06-10 23:43:37 +0530

    [Tushar Vidwans] Sorting the array using merge sort

---
+++ MergeSort.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort_Day16_17
+{
+    public class MergeSort1
+    {
+        /// <summary>
+        /// In this method i'm sorting the array using merge sort.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+
+        public int[] mergeSort(int[] array)
+        {
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+            if (array.Length <= 1)
+                return array;
+            int midPoint = array.Length / 2;
+            left = new int[midPoint];
+
+            if (array.Length % 2 == 0)
+                right = new int[midPoint];
+            else
+                right = new int[midPoint + 1];
+            for (int i = 0; i < midPoint; i++)
+                left[i] = array[i];
+            int x = 0;
+            for (int i = midPoint; i < array.Length; i++)
+            {
+                right[x] = array[i];
+                x++;
+            }
+            left = mergeSort(left);
+            right = mergeSort(right);
+            result = merge(left, right);
+            return result;
+        }
+
+        public int[] merge(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+            return result;
+        }
+
+        public void printArray(int[] arr)
+        {
+
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MergeSort_Day16_17
+{
+
+    /// <summary>
+    /// this is the driver method in which i'm calling the merge method.
+    /// </summary>
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] ints = new int[] { 55, 76, 23, 98, 32, 1, 0, 43, 56 };
+            MergeSort1 mergeSort = new MergeSort1();
+
+            Console.WriteLine("Before Merge Sort: ");
+            mergeSort.printArray(ints);
+            int[] sorted= mergeSort.mergeSort(ints);
+            Console.WriteLine("After Merger sort");
+            mergeSort.printArray(sorted);
+        }
+
+        
+    }
+}
